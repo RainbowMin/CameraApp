0d2d8576b3d70dbf586edd40c98be6105be01dc9
RainbowMin <minjie@minjiedeMacBook-Air.local>
2014-07-06 20:14:45 +0800

    添加了两个package：2d platform , iOS basic, 并初步实现了涂鸦拍照功能

---
+++ Assets/CameraApp/Scripts/DrawTextureTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawTextureTest : MonoBehaviour {
+
+	//public Texture2D _texture;
+	//public Rect _rect;
+	public CameraPreviewVideo _cameraVideo;
+	public GameObject _object;
+
+	// Use this for initialization
+	void Start () 
+	{
+		_cameraVideo = gameObject.GetComponent<CameraPreviewVideo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MeshRenderer renderer = _object.GetComponent<MeshRenderer> ();
+		renderer.material.mainTexture = _cameraVideo.webCamTexture;
+
+	}
+
+	void OnGUI()
+	{
+		//GUI.DrawTexture (_rect, _texture);
+	}
+}
---
+++ Assets/CameraApp/Scripts/ShowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCamera : MonoBehaviour {
+
+	public Texture2D _texture;
+	public GameObject _object;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MeshRenderer renderer = _object.GetComponent<MeshRenderer> ();
+		renderer.material.mainTexture = _texture;
+	}
+}
